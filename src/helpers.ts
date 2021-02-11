57bebe3b6adfd8ad48e37524e4a31c37892a0ebf
Michael <micjanic20@gmail.com>
2021-02-10 22:03:46 -0600

    create bricks array

--- src/helpers.ts
+++ src/helpers.ts
@@ -10,4 +10,25 @@ import {
     BRICK_ENERGY,
 } from './setup'
 
-export function createBricks(): Brick[] {}
+export function createBricks(): Brick[] {
+    return LEVEL.reduce((acc, element, i) => {
+        const row = Math.floor(i + 1) / STAGE_COLS
+        const col = i % STAGE_COLS
+
+        const x = STAGE_PADDING + col * (BRICK_WIDTH + BRICK_PADDING)
+        const y = STAGE_PADDING + row * (BRICK_HEIGHT + BRICK_PADDING)
+
+        if (element === 0) return acc
+
+        return [
+            ...acc,
+            new Brick(
+                BRICK_WIDTH,
+                BRICK_HEIGHT,
+                { x, y },
+                BRICK_ENERGY[element],
+                BRICK_IMAGES[element]
+            ),
+        ]
+    }, [] as Brick[])
+}
