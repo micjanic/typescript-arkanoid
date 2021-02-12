1d962bd24e09e0126525c72a6dfde1d289c57ab4
Michael <micjanic20@gmail.com>
2021-02-11 22:37:18 -0600

    bug fix

--- src/helpers.ts
+++ src/helpers.ts
@@ -13,11 +13,13 @@ import {
 
 export function createBricks(): Brick[] {
     return LEVEL.reduce((acc, element, i) => {
-        const row = Math.floor(i + 1) / STAGE_COLS
+        const row = Math.floor((i + 1) / STAGE_COLS)
         const col = i % STAGE_COLS
+        debugger
 
         const x = STAGE_PADDING + col * (BRICK_WIDTH + BRICK_PADDING)
         const y = STAGE_PADDING + row * (BRICK_HEIGHT + BRICK_PADDING)
+        debugger
 
         if (element === 0) return acc
 
