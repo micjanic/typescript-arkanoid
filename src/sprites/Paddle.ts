d727edd6bfd8508684d254c62a34accb694b387e
Michael <micjanic20@gmail.com>
2021-02-13 14:30:46 -0600

    paddle getters and move method

--- src/sprites/Paddle.ts
+++ src/sprites/Paddle.ts
@@ -18,6 +18,39 @@ export class Paddle {
         this.position = position
         this.moveLeft = false
         this.moveRight = false
-        this.paddleImage = image
+        this.paddleImage.src = image
+
+        // Event listeners
+        document.addEventListener('keydown', this.handleKeyDown)
+        document.addEventListener('keyup', this.handleKeyUp)
+    }
+
+    get width(): number {
+        return this.paddleWidth
+    }
+
+    get height(): number {
+        return this.paddleHeight
+    }
+
+    get pos(): Vector {
+        return this.position
+    }
+
+    get image(): HTMLImageElement {
+        return this.paddleImage
+    }
+
+    get isMovingLeft(): boolean {
+        return this.moveLeft
+    }
+
+    get isMovingRight(): boolean {
+        return this.moveRight
+    }
+
+    movePaddle(): void {
+        if (this.moveLeft) this.pos.x -= this.speed
+        if (this.moveRight) this.pos.x += this.speed
     }
 }
