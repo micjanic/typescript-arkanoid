98f67c500ac4cd36b3aea9dba9dac22822932063
Michael <micjanic20@gmail.com>
2021-02-13 14:26:28 -0600

    set up paddle constructor

--- src/sprites/Paddle.ts
+++ src/sprites/Paddle.ts
@@ -1,3 +1,23 @@
 import { Vector } from '../types'
 
-export class Paddle {}
+export class Paddle {
+    private paddleImage: HTMLImageElement = new Image()
+    private moveLeft: boolean
+    private moveRight: boolean
+
+    constructor(
+        private speed: number,
+        private paddleWidth: number,
+        private paddleHeight: number,
+        private position: Vector,
+        image: string
+    ) {
+        this.speed = speed
+        this.paddleWidth = paddleWidth
+        this.paddleHeight = paddleHeight
+        this.position = position
+        this.moveLeft = false
+        this.moveRight = false
+        this.paddleImage = image
+    }
+}
