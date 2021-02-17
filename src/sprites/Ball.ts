829d61fe3a8ce9c183866b11d629dc21c32ae2fc
Michael <micjanic20@gmail.com>
2021-02-16 21:06:45 -0600

    ball constructor and getters

--- src/sprites/Ball.ts
+++ src/sprites/Ball.ts
@@ -1,3 +1,38 @@
 import { Vector } from '../types'
 
-export class Ball {}
+export class Ball {
+    private speed: Vector;
+    private ballImage: HTMLImageElement = new Image(),
+    
+    constructor(
+        private ballSize: number,
+        private position: Vector,
+        speed: number,
+        image: string
+    ){
+        this.ballSize = ballSize;
+        this.position = position;
+        this.speed = {
+            x: speed,
+            y: -speed
+        }
+        this.ballImage.src = image
+    }
+
+    // Getters
+    get width(): number {
+        return this.ballSize;
+    }
+
+    get height(): number {
+        return this.ballSize;
+    }
+
+    get pos(): Vector {
+        return this.position;
+    }
+
+    get image(): HTMLImageElement {
+        return this.ballImage;
+    }
+}
